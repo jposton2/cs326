025753423aed8213839a95aecb7ab9a11f5d857a
Lucas Stuyvesant <lplayer12893@aol.com>
2014-03-25 20:49:18 -0700

    Comment Grid
    ^

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Grid.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Grid.java
@@ -3,17 +3,26 @@ package com.cs326.team5.qr_labyrinth;
 import java.util.ArrayList;
 
 public class Grid{
+	
     private ArrayList<ArrayList<PointData>> grid;
-    Point start;
-    Grid(Point start, ArrayList<ArrayList<PointData>> grid){
+    private Point start;
+    
+    public Grid(Point start, ArrayList<ArrayList<PointData>> grid){
         this.grid = grid;
         this.start = start;
     }
-    ArrayList<ArrayList<PointData>> getGrid(){
+    
+    /**
+     * @return grid
+     */
+    public ArrayList<ArrayList<PointData>> getGrid(){
         return grid;
     }
 
-    Point getStart(){
+    /**
+     * @return start point
+     */
+    public Point getStart(){
         return start;
     }
 }
