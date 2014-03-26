5e1bc31a09f3d58ce96a8923ce63a5e79415d536
jposton2 <jposton@cs.nmt.edu>
2014-03-25 20:34:46 -0600

    Aded basic framework stuff

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/GridHandler.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/GridHandler.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class GridHandler{
     private int minSize;
     private Grid grid;
-    GridHandler(Grid g, int minSize){
+    private ArrayList<ArrayList<Point>> subgraphs;
+    GridHandler(Grid g, int minSize, ArrayList<ArrayList<Point>> subgraphs){
+        this.subgraphs = subgraphs;
+        this.minSize = minSize;
         grid = g;
     }
     public Grid getGrid(){
         return grid;
     }
-
+    public Vector<Point> getFurthestPints(ArrayList<Point> subgrid){
+        return null;
+    }
+    public void setTeleporters(ArrayList<ArrayList<Point>> subgrids){
+        // TODO:Add teleporter setters 
+    }
+    ArrayList<ArrayList<Point>> QRTo2DArray(){
+        // TODO: Implement
+        return subgraphs;
+    }
 
 
 }
