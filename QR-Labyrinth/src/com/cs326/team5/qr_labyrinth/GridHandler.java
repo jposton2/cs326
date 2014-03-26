11b166156ea8bda0ab8aa29f884fee89704a5de9
Lucas Stuyvesant <lplayer12893@aol.com>
2014-03-25 20:53:32 -0700

    Commented GridHandler
    ^

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/GridHandler.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/GridHandler.java
@@ -4,24 +4,43 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class GridHandler{
+	
     private int minSize;
     private Grid grid;
     private ArrayList<ArrayList<PointData>> subgraphs;
-    GridHandler(Grid g, int minSize, ArrayList<ArrayList<PointData>> subgraphs){
+    
+    public GridHandler(Grid g, int minSize, ArrayList<ArrayList<PointData>> subgraphs){
         this.subgraphs = subgraphs;
         this.minSize = minSize;
         grid = g;
     }
+    
+    /**
+     * @return grid
+     */
     public Grid getGrid(){
         return grid;
     }
-    public Vector<Point> getFurthestPints(ArrayList<Point> subgrid){
+    
+    /**
+     * @param subgrid
+     * @return points furthest from each other in the subgrid
+     */
+    public Vector<Point> getFurthestPoints(ArrayList<Point> subgrid){
         return null;
     }
+    
+    /**
+     * @param subgrids
+     */
     public void setTeleporters(ArrayList<ArrayList<Point>> subgrids){
         // TODO:Add teleporter setters 
     }
-    ArrayList<ArrayList<PointData>> QRTo2DArray(){
+    
+    /**
+     * @return 2D array of PointData representing a QR code
+     */
+    public ArrayList<ArrayList<PointData>> QRTo2DArray(){
         // TODO: Implement
         return subgraphs;
     }
