01a8f14c1b35860f285e1c9758adc9afa20ec270
Lucas Stuyvesant <lplayer12893@aol.com>
2014-03-25 20:58:44 -0700

    Commented Point and PointData and added whitespace
    ^

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Point.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Point.java
@@ -1,15 +1,25 @@
 package com.cs326.team5.qr_labyrinth;
 
 public class Point{
+	
     private int x, y;
-    Point(int x, int y){
+    
+    public Point(int x, int y){
         this.x = x;
         this.y = y;
     }
-    int getX(){
+    
+    /**
+     * @return x coordinate
+     */
+    public int getX(){
         return x;
     }
-    int getY(){
+    
+    /**
+     * @return y coordinate
+     */
+    public int getY(){
         return y;
     }
 }
--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/PointData.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/PointData.java
@@ -1,15 +1,30 @@
 package com.cs326.team5.qr_labyrinth;
 
 public class PointData{
+	
     private boolean black;
-    Point destination;
-    PointData(Point p, boolean b){
+    
+    private Point destination;
+
+    public PointData(boolean b){
+    	this(null, b);
+    }
+    
+    public PointData(Point p, boolean b){
         destination = p;
         black = b;
     }
+    
+    /**
+     * @return true if represents a black point, false if white
+     */
     public boolean isBlack(){
         return black;
     }
+    
+    /**
+     * @return destination point
+     */
     public Point getDestination(){
         return destination;
     }
