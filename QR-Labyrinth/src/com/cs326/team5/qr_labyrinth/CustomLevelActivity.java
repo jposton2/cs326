6dd5e9b2d348f72c676aa56a6f1ca52ea424cea9
jposton2 <jposton@cs.nmt.edu>
2014-04-15 21:58:53 -0600

    MORE CHANGES!!!! GAHHHHHHHHHHH

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/CustomLevelActivity.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/CustomLevelActivity.java
@@ -1,5 +1,12 @@
 package com.cs326.team5.qr_labyrinth;
 
-public class CustomLevelActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class CustomLevelActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
 }
