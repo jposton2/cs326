974fa1232424d327e9d308ed1fb61253b11acafe
thegui <tguinard@nmt.edu>
2014-04-18 19:34:55 -0600

    fixed teleporting issues

--- QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Subgrid.java
+++ QR-Labyrinth/src/com/cs326/team5/qr_labyrinth/Subgrid.java
@@ -1,19 +1,31 @@
 package com.cs326.team5.qr_labyrinth;
 
+import android.annotation.SuppressLint;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * Area of whitespace in QR grid
+ *
+ */
 public class Subgrid {
 		
 	Grid grid;
 	int groupID;
 	ArrayList<PointData> subgridArray = new ArrayList<PointData>();
 	
-	HashMap<Integer, PointData> teleporterDestinations;
-	ArrayList<PointData> teleporters;
+	//maps subgrid ids to locations of teleporters
+	@SuppressLint("UseSparseArrays")
+	HashMap<Integer, PointData> teleporterDestinations = new HashMap<Integer, PointData>();
+	ArrayList<PointData> teleporters = new ArrayList<PointData>();
+	
+	//pseudo teleporter:
+	//multiple teleporters can have this as a target
+	//default destination is grid.deadEnd
+	//will return to the node used to get there
 	private PointData pseudoTeleporter = null;
 	
 	public Subgrid(Grid grid, int groupId){
@@ -21,10 +33,15 @@ public class Subgrid {
 		this.groupID = groupId;
 	}
 	
-	public void addTeleporter(int x, int y){
-		this.teleporters.add(grid.getGrid()[x][y]);
-	}
-	
+	/**
+	 * Perform breadth first search 
+	 * used to find teleporter that is mutally farthest away
+	 * from set teleporters
+	 * 
+	 * @param teleporters
+	 * @param nodes
+	 * @return
+	 */
 	private LinkedList<PointData> multiBFS(Collection<PointData> teleporters, Collection<PointData> nodes){
 		HashMap<PointData, Boolean> isEnqueued = new HashMap<PointData, Boolean>();
 		LinkedList<PointData> toVisit = new LinkedList<PointData>();
@@ -45,7 +62,7 @@ public class Subgrid {
 			visited.add(visiting);
 			for(PointData pd: grid.getWhiteNeighbors(visiting)){
 				if(isEnqueued.get(pd) != null && !isEnqueued.get(pd)){
-					isEnqueued.put(pd, false);
+					isEnqueued.put(pd, true);
 					toVisit.add(pd);
 				}
 			}
@@ -54,18 +71,29 @@ public class Subgrid {
 		return visited;
 	}
 	
+	/**
+	 * Determine if it is safe to add a teleporter at specified point
+	 * This means no teleporters are adjacent to it.
+	 * 
+	 * @param teleportAdd
+	 * @return
+	 */
 	private boolean isSafe(PointData teleportAdd){
-		if(teleportAdd.hasTeleporter()){
+		if(teleportAdd.hasTeleporter() || teleportAdd.isPseudoTeleporter()){
 			return false;
 		}
 		for(PointData p : grid.getWhiteNeighbors(teleportAdd)){
-			if(p.hasTeleporter()){
+			if(p.hasTeleporter() || p.isPseudoTeleporter()){
 				return false;
 			}
 		}
 		return true;
 	}
 	
+	/**
+	 * Determine if it is possible to add a teleporter to the subgrid
+	 * @return
+	 */
 	public boolean canAddTeleporter(){
 		for(PointData pd : this.subgridArray){
 			if(this.isSafe(pd)){
@@ -75,24 +103,39 @@ public class Subgrid {
 		return false;
 	}
 	
+	/**
+	 * Add a single teleporter to the subgrid
+	 * 
+	 * @param destinationId
+	 * @return
+	 */
 	private boolean addTeleporter(int destinationId){
+		//first teleporter added
 		if(this.teleporters.size() == 0 && this.subgridArray.size() > 0){
 			teleporters.add(this.subgridArray.get(0));
+			teleporterDestinations.put(destinationId, null);
+			this.subgridArray.get(0).setTeleporter(true);
 			return true;
 		}
-		
+
 		LinkedList<PointData> visitOrder = this.multiBFS(teleporters, this.subgridArray);
 		while(!visitOrder.isEmpty()){
 			PointData candidate = visitOrder.removeLast();
 			if(this.isSafe(candidate)){
 				teleporters.add(candidate);
 				teleporterDestinations.put(destinationId, null);
+				candidate.setTeleporter(true);
 				return true;
 			}
 		}
 		return false;
 	}
 	
+	/**
+	 * Add a teleporter to subgrid
+	 * @param destination
+	 * @return
+	 */
 	public boolean addTeleporter(Subgrid destination){
 		return this.addTeleporter(destination.groupID);
 	}
@@ -105,19 +148,19 @@ public class Subgrid {
 	}
 	
 	/**
-	 * @param pseudoTeleporter the pseudoTeleporter to set
+	 * Add a pseudo teleporter if one does not exist
+	 * @return
 	 */
-	public void setPseudoTeleporter(PointData pseudoTeleporter) {
-		this.pseudoTeleporter = pseudoTeleporter;
-	}
-	
 	public boolean addPseudoTeleporter(){
 		if(this.pseudoTeleporter != null){
 			return true;
 		}
 		if(this.addTeleporter(-1)){
+			this.pseudoTeleporter = teleporters.get(teleporters.size() - 1);
 			this.teleporters.remove(teleporters.size() - 1);
-			this.setPseudoTeleporter(teleporters.get(teleporters.size() - 1));
+			this.teleporterDestinations.remove(-1);
+			this.pseudoTeleporter.setPseudoTeleporter(true);
+			this.pseudoTeleporter.setTeleporter(false);
 			return true;
 		}
 		else{
@@ -125,6 +168,10 @@ public class Subgrid {
 		}
 	}
 
+	/**
+	 * Assign teleporter destinations.  
+	 * Do this after all teleporters have been added
+	 */
 	public void assignTeleporterDestinations(){
 		Collections.shuffle(teleporters);
 		int i = 0;
@@ -132,5 +179,10 @@ public class Subgrid {
 			teleporterDestinations.put(destination, teleporters.get(i));
 			++i;
 		}
+	}
+	
+	@Override
+	public boolean equals(Object that){
+		return this.groupID == ((Subgrid) that).groupID;
 	}
 }
